617b170c277b2ae5962abe38d47f5380ec1ec4d7
Rodrigo <rodrigo.blaustein@gmail.com>
2012-11-24 11:14:48 -0800

    moved the code form the renderMap.js to the Mapwidget file

--- UI/htdocs/static/js/RenderMap.js
+++ UI/htdocs/static/js/RenderMap.js
@@ -1,70 +1,9 @@
-// Start position for the map (hardcoded here for simplicity)
-        var lat = 49.27628;
-        var lon = -123.17561;
-        var zoom = 12;
 
-        var map; //complex object of type OpenLayers.Map
-
-        //Initialise the 'map' object
-        function init() {
-
-            map = new OpenLayers.Map("map", {
-                controls: [
-                    new OpenLayers.Control.Navigation(),
-                    new OpenLayers.Control.ScaleLine({ geodesic: true }),
-                    new OpenLayers.Control.MousePosition(),],
-                maxExtent: new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34),
-                maxResolution: 156543.0339,
-                numZoomLevels: 19,
-                units: 'm',
-                projection: new OpenLayers.Projection("EPSG:900913"),
-                displayProjection: new OpenLayers.Projection("EPSG:4326")
-            });
-
-			/*uncomment below to put in map from web*/			
-//  			layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-//  			layerMapnik.setOpacity(0.4);
-//  			map.addLayer(layerMapnik);						
-//  			var switcherControl = new OpenLayers.Control.LayerSwitcher();
-//  			map.addControl(switcherControl);
-//  			switcherControl.maximizeControl();
-
-            // This is the layer that uses the locally stored tiles
-            var newLayer = new OpenLayers.Layer.OSM("Local Tiles", "static/tiles/${z}/${x}/${y}.png", { numZoomLevels: 19, alpha: true, isBaseLayer: true });
-            map.addLayer(newLayer);
-            // This is the end of the layer
-
-
-            if (!map.getCenter()) {
-                var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
-                map.setCenter(lonLat, zoom);
-            }
-
-            //we create a new marker layer and add it to the map
-            var markers = new OpenLayers.Layer.Markers("Markers");
-            map.addLayer(markers);
-                                 
-            //now we call the add_marker function for adding a new marker to the marker_layer, we set it up to be located at lon-lat for testing.
-            var marker = add_marker(markers, lon, lat);
             
-            //this is how we can delete previously created markers.
-            //marker.destroy();
-        }
+        
 
 
-        //Adds a marker layer to the map and calls addMarker.        
-        function add_marker(markers, lon, lat) {
-                                           
-            //here we define all the properties of the icon for the marker
-            var size = new OpenLayers.Size(21, 25);
-            var offset = new OpenLayers.Pixel(-(size.w / 2), -size.h);
-            var icon = new OpenLayers.Icon("static/img/map/marker.png", size, offset);
-
-            var marker = new OpenLayers.Marker(new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()), icon);
-            markers.addMarker(marker);
-
-            return marker;                       
-        }
+      
 
 
     
