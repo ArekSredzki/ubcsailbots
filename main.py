942022d678c083a23d71cef6e78027a2e1cc50ff
DavidRLee <davidlee.drl@gmail.com>
2013-02-13 16:41:21 -0800

    moving server.py back into ui folder where it belongs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import server
+import ui.server
 import thread
 from ui.api import ApiControl
 from ui.simulator import Simulator
 interface = Simulator()
-server.apiControl = ApiControl(interface)
-thread.start_new_thread(server.app.run())
+ui.server.apiControl = ApiControl(interface)
+thread.start_new_thread(ui.server.app.run())
