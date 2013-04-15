a852432326674eb3bb2a0a75db5c508afbe919a1
Josh Andrews <joshphilipandrews@gmail.com>
2013-04-14 19:46:17 -0700

    removed unnecessary variables

--- control/StaticVars.py
+++ control/StaticVars.py
@@ -45,15 +45,3 @@ ACCEPTANCE_DISTANCE_DEFAULT = 3     #The acceptable distance (how close the boat
 AWA_THRESHOLD = 0.9         #Since the table has non-realistic values for AWA, this allows the AWA lookup to be off slightly
 SOG_THRESHOLD = 0.9
 SPEED_AFFECTION_THRESHOLD = 30     #Speed threshold at which lower speeds are shown to note have a significant variation between AWA and TWA
-
-# Indices for current_data
-HOG_INDEX=0     # Heading over Ground
-COG_INDEX=1     # Course over Ground
-SOG_INDEX=2     # Speed over Ground
-AWA_INDEX=3     # Apparent Wind Angle Average
-GPS_INDEX=4     # GPS Coordinate
-SHT_INDEX=5     # Sheet Percentage
-SAT_INDEX=6     # GPS Number of Satellites
-ACC_INDEX=7     # GPS Accuracy (HDOP)
-AUT_INDEX=8     # Auto Mode
-RUD_INDEX=9     # Rudder
