9c6ff1a68b82e7bb18a7a72a7c37838a787b919c
Josh Andrews <joshphilipandrews@gmail.com>
2013-04-14 19:43:40 -0700

    speed affection theshold change

--- control/StaticVars.py
+++ control/StaticVars.py
@@ -43,8 +43,8 @@ SK_BOTTOM_RIGHT = "sk_bottom_right"
 # Thresholds for sailing logic functions
 ACCEPTANCE_DISTANCE_DEFAULT = 3     #The acceptable distance (how close the boat has to be to the waypoint before it accepts that it has reached its destination
 AWA_THRESHOLD = 0.9         #Since the table has non-realistic values for AWA, this allows the AWA lookup to be off slightly
-SOG_THRESHOLD = 0.5
-SPEED_AFFECTION_THRESHOLD = 55     #Speed threshold at which lower speeds are shown to note have a significant variation between AWA and TWA
+SOG_THRESHOLD = 0.9
+SPEED_AFFECTION_THRESHOLD = 30     #Speed threshold at which lower speeds are shown to note have a significant variation between AWA and TWA
 
 # Indices for current_data
 HOG_INDEX=0     # Heading over Ground
