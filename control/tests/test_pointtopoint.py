056b0b2bfc8b3627824858c6661aab07f70e4a3e
DavidRLee <davidlee.drl@gmail.com>
2013-04-19 15:31:54 -0700

    adding more tests

--- control/tests/test_pointtopoint.py
+++ control/tests/test_pointtopoint.py
@@ -6,37 +6,51 @@ from control import GlobalVars
 print sys.path
 from control.logic import pointtopoint
 from control import sailbotlogger
+import control.datatype.datatypes as datatypes
 
 class TestPointToPoint(unittest.TestCase):
   def setUp(self):
     GlobalVars.logger = sailbotlogger.logger()
     self.p2p = pointtopoint.PointToPoint()
       
-  def testStarboardTackWanted(self):
+  def testWhichTackWanted(self):
     initialTack=None
     self.p2p.AWA = 30
     self.assertFalse(self.p2p.starboardTackWanted(initialTack))
-    self.p2p.AWA = -30
-    self.assertTrue(self.p2p.starboardTackWanted(initialTack))
-    self.p2p.AWA = -150
-    self.assertTrue(self.p2p.starboardTackWanted(initialTack))
-    self.p2p.AWA = 150
-    self.assertFalse(self.p2p.starboardTackWanted(initialTack))
-    self.p2p.AWA = 0
-    self.assertFalse(self.p2p.starboardTackWanted(initialTack))
+    self.assertTrue(self.p2p.portTackWanted(initialTack))
 
-  def testPortTackWanted(self):
-    initialTack=None
-    self.p2p.AWA = 30
-    self.assertTrue(self.p2p.portTackWanted(initialTack))
     self.p2p.AWA = -30
+    self.assertTrue(self.p2p.starboardTackWanted(initialTack))
     self.assertFalse(self.p2p.portTackWanted(initialTack))
+
     self.p2p.AWA = -150
+    self.assertTrue(self.p2p.starboardTackWanted(initialTack))
     self.assertFalse(self.p2p.portTackWanted(initialTack))
+
     self.p2p.AWA = 150
+    self.assertFalse(self.p2p.starboardTackWanted(initialTack))
     self.assertTrue(self.p2p.portTackWanted(initialTack))
+
     self.p2p.AWA = 0
+    self.assertFalse(self.p2p.starboardTackWanted(initialTack))
     self.assertTrue(self.p2p.portTackWanted(initialTack))
 
   def testdoWeStillWantToTack(self):
+    self.p2p.GPSCoord = datatypes.GPSCoordinate(49,-123)
+    self.p2p.Dest = datatypes.GPSCoordinate(49.1,-123) # 0 degrees, N
+    
+    self.p2p.hog = 0 # N
+    self.assertTrue(self.p2p.doWeStillWantToTack())
+    
+    self.p2p.hog = 45 # NE
+    self.assertTrue(self.p2p.doWeStillWantToTack())
+    
+    self.p2p.hog = 90 # E
+    self.assertFalse(self.p2p.doWeStillWantToTack())
+    
+    self.p2p.hog = -45 # NW
+    self.assertTrue(self.p2p.doWeStillWantToTack())
+    
+    self.p2p.hog = -90 # NW
+    self.assertFalse(self.p2p.doWeStillWantToTack())
     
