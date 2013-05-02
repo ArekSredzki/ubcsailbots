347acdd8fd948066af61d620c507fcb1c6fe6664
Michael Peterson <peterson_michael_j@hotmail.com>
2013-05-02 12:44:10 -0700

    Added an initial sail point for better rounding of buoy

--- control/logic/roundbuoy.py
+++ control/logic/roundbuoy.py
@@ -14,7 +14,9 @@ from control import sailing_task
 class RoundBuoy(sailing_task.SailingTask):
     #Constants
     TargetAndBuoyAngle = 45
-    TargetToBuoyDist = math.sqrt(50)
+    InitialSailAndBuoyAngle = 135
+    TargetToBuoyDist = math.sqrt(math.pow(5,2)+math.pow(5,2))
+    InitialSailToBuoyDist = math.sqrt(math.pow(7,2)+math.pow(7,2))
     MinimumDistance = 10
     
     def __init__(self):
@@ -23,21 +25,29 @@ class RoundBuoy(sailing_task.SailingTask):
     def run(self, BuoyLoc, FinalLoc=None, port=True):
         rightBuoyPoint = self.findRightBuoyPoint(BuoyLoc)
         leftBuoyPoint = self.findLeftBuoyPoint(BuoyLoc)
+        rightInitialPoint = self.findRightInitialPoint(BuoyLoc)
+        leftInitialPoint = self.findLeftInitialPoint(BuoyLoc)
+        
         if FinalLoc is None:
             FinalLoc = datatypes.GPSCoordinate()
             FinalLoc.lat = gVars.currentData.gps_coord.lat
             FinalLoc.long = gVars.currentData.gps_coord.long
-            
+                        
         if(self.distanceBetweenBoatAndBuoyGreaterThanMinDistance(BuoyLoc)):
-            self.pointtopoint.run(BuoyLoc,None,self.MinimumDistance)
+            if(port):
+                gVars.logger.info("Sailing To Right Initial Point: " + repr(rightInitialPoint))
+                self.pointtopoint.run(rightInitialPoint)
+            else:
+                gVars.logger.info("Sailing To Left Initial Point: " + repr(leftInitialPoint))
+                self.pointtopoint.run(leftInitialPoint)
             
         if(port==True):
             gVars.logger.info("Sailing To Right Buoy Point: " + repr(rightBuoyPoint))
             self.pointtopoint.run(rightBuoyPoint, 0)
-            gVars.logger.info("Final Loc is:" + repr(FinalLoc))
+            #gVars.logger.info("Final Loc is:" + repr(FinalLoc))
             gVars.logger.info("Sailing To Left Buoy Point: " + repr(leftBuoyPoint))
             self.pointtopoint.run(leftBuoyPoint, None, None, True)
-            gVars.logger.info("Final Loc is:" + repr(FinalLoc))
+            #gVars.logger.info("Final Loc is:" + repr(FinalLoc))
         else:
             gVars.logger.info("Sailing To Left Buoy Point: " + repr(leftBuoyPoint))
             self.pointtopoint.run(leftBuoyPoint, 1)
@@ -67,6 +77,28 @@ class RoundBuoy(sailing_task.SailingTask):
         leftPoint = standardcalc.GPSDistAway(BuoyLoc, LongitudalDistBetweenBuoyAndTarget, LatitudalDistBetweenBuoyAndTarget)
         
         return leftPoint
+    
+    def findRightInitialPoint(self, BuoyLoc):
+        angleOfCourse = standardcalc.angleBetweenTwoCoords(gVars.currentData.gps_coord, BuoyLoc)
+        angleFromEast = 90-angleOfCourse
+        
+        LongitudalDistBetweenBuoyAndInitialSailPoint = self.InitialSailToBuoyDist*math.cos(math.radians(angleFromEast-self.InitialSailAndBuoyAngle))
+        LatitudalDistBetweenBuoyAndInitialSailPoint = self.InitialSailToBuoyDist*math.sin(math.radians(angleFromEast-self.InitialSailAndBuoyAngle))
+        
+        rightInitialPoint = standardcalc.GPSDistAway(BuoyLoc, LongitudalDistBetweenBuoyAndInitialSailPoint, LatitudalDistBetweenBuoyAndInitialSailPoint)
+        
+        return rightInitialPoint
+    
+    def findLeftInitialPoint(self, BuoyLoc):
+        angleOfCourse = standardcalc.angleBetweenTwoCoords(gVars.currentData.gps_coord, BuoyLoc)
+        angleFromEast = 90-angleOfCourse
+        
+        LongitudalDistBetweenBuoyAndInitialSailPoint = self.InitialSailToBuoyDist*math.cos(math.radians(angleFromEast+self.InitialSailAndBuoyAngle))
+        LatitudalDistBetweenBuoyAndInitialSailPoint = self.InitialSailToBuoyDist*math.sin(math.radians(angleFromEast+self.InitialSailAndBuoyAngle))
+        
+        leftInitialPoint = standardcalc.GPSDistAway(BuoyLoc, LongitudalDistBetweenBuoyAndInitialSailPoint, LatitudalDistBetweenBuoyAndInitialSailPoint)
+        
+        return leftInitialPoint
     
     def distanceBetweenBoatAndBuoyGreaterThanMinDistance(self,BuoyLoc):
         if(standardcalc.distBetweenTwoCoords(gVars.currentData.gps_coord,BuoyLoc)>self.MinimumDistance):
