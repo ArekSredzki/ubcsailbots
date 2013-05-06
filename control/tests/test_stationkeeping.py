1def0615c18327da1d750d2038f3e86ed6472663
DavidRLee <davidlee.drl@gmail.com>
2013-05-06 16:01:53 -0700

    more unit tests for SK

--- control/tests/test_stationkeeping.py
+++ control/tests/test_stationkeeping.py
@@ -14,7 +14,6 @@ from control.logic import standardcalc
 
 class TestGetStartingDirection(unittest.TestCase):
     def setUp(self):
-        gVars.logger = sailbot_logger.Logger()
         self.stationKeeping = stationkeeping.StationKeeping()
         topLeftCoord= datatypes.GPSCoordinate(50,-123)
         topRightCoord=datatypes.GPSCoordinate(50,-122)
@@ -34,6 +33,32 @@ class TestGetStartingDirection(unittest.TestCase):
         startDirection = self.stationKeeping.getStartDirection(self.waypoints)
         self.assertEqual(startDirection,3)
 
+class TestWaypointCoords(unittest.TestCase):
+  def setUp(self):
+    self.stationKeeping = stationkeeping.StationKeeping()
+    topLeftCoord= datatypes.GPSCoordinate(50,-123)
+    topRightCoord=datatypes.GPSCoordinate(50,-122)
+    botLeftCoord=datatypes.GPSCoordinate(49,-123)
+    botRightCoord=datatypes.GPSCoordinate(49,-122)
+    self.boxCoords = standardcalc.setBoxCoords(topLeftCoord, topRightCoord, botLeftCoord, botRightCoord)  
+    self.waypoints = self.stationKeeping.setWayPtCoords(self.boxCoords)
+  def testNorthWpt(self):
+    northWpt = datatypes.GPSCoordinate(50,-122.5)
+    self.assertEqual(self.waypoints[0].lat,northWpt.lat)
+    self.assertEqual(self.waypoints[0].long,northWpt.long) 
+  def testEastWpt(self):
+    eastWpt = datatypes.GPSCoordinate(49.5,-122)
+    self.assertEqual(self.waypoints[1].lat,eastWpt.lat)
+    self.assertEqual(self.waypoints[1].long,eastWpt.long)     
+  def testSouthWpt(self):
+    southWpt = datatypes.GPSCoordinate(49,-122.5)
+    self.assertEqual(self.waypoints[2].long,southWpt.long)         
+    self.assertEqual(self.waypoints[2].lat,southWpt.lat)
+  def testWestWpt(self):
+    eastWpt = datatypes.GPSCoordinate(49.5,-123)
+    self.assertEqual(self.waypoints[3].lat,eastWpt.lat)
+    self.assertEqual(self.waypoints[3].long,eastWpt.long)             
+      
 @unittest.skip("skip while testing above code")      
 class TestStationKeeping(unittest.TestCase):
     def setUp(self):
