4e26320514514bc269d5d09b869a63e0227ea7d9
Josh Andrews <joshphilipandrews@gmail.com>
2013-05-16 12:46:48 -0700

    removed unused datatypes

--- control/tests/test_datatypes.py
+++ control/tests/test_datatypes.py
@@ -2,7 +2,6 @@
 import sys, os
 sys.path.append(os.path.abspath('../'))
 import unittest
-import math
 import control.datatype.datatypes as datatype
 import control.static_vars as sVars
 
@@ -16,57 +15,6 @@ class TestGPSCoordinate(unittest.TestCase):
 		self.assertEqual(self.x.long, 0)
 		self.assertEqual(self.y.lat, 42)
 		self.assertEqual(self.y.long, -121)
-		
-class TestBoundInt(unittest.TestCase):
-	def setUp(self):
-		self.x = datatype.BoundInt()
-		self.y = datatype.BoundInt(-20,-10,10)
-
-	def test_constructor(self):
-		self.assertEqual(self.x.value(), 0)
-		self.assertEqual(self.y.value(), -10)
-
-	def test_outofboundassignment(self):
-		self.y.set(20)
-		self.assertEqual(self.y.value(), 10)
-
-	def test_changebounds(self):
-		self.y.setBound(0,10)
-		self.y.set(-10)
-		self.assertEqual(self.y.value(), 0)
-
-		self.y.setLowerBound(5)
-		self.assertEqual(self.y.value(), 5)
-
-		self.y.setUpperBound(8)
-		self.y.set(10)
-		self.assertEqual(self.y.value(), 8)
-
-class testAngle(unittest.TestCase):
-	def setUp(self):
-		self.default = datatype.Angle()
-		self.x = datatype.Angle(10)
-		self.y = datatype.Angle(-10)
-
-	def test_constructor(self):
-		self.assertEqual(self.default.degrees(), 0)
-		self.assertEqual(self.x.degrees(), 10)
-		self.assertEqual(self.y.degrees(), -10)
-
-	def test_radianConversion(self):
-		self.assertEqual(self.default.radians(), math.radians(0))
-		self.assertEqual(self.x.radians(), math.radians(10))
-		self.assertEqual(self.y.radians(), math.radians(-10))
-
-	def test_wraparound(self):
-		self.assertEqual(datatype.Angle(740).degrees(), 20)
-		self.assertEqual(datatype.Angle(-400).degrees(), -40)
-
-	def test_operators(self):
-		self.assertEqual((self.x + 20).degrees(), 30)
-		self.assertEqual((20 + self.x).degrees(), 30)
-		self.assertEqual((self.x + 360).degrees(), 10)
-		self.assertEqual((self.x - 360).degrees(), 10)
 
 class TestWaypoint(unittest.TestCase):
 	def setUp(self):
